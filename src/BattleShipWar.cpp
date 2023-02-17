2ed14ba096a55715c9362d523431fbe8ed708056
Emir Han Mamak <emirhanmamak6@gmail.com>
2023-02-17 10:05:07 +0300

    Project Created

---
+++ src/BattleShipWar.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BattleShipWar.cpp
+// Author      : Emir Han Mamak
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "BattleShipWar" << endl; // prints BattleShipWar
+	return 0;
+}
